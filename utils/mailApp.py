941f3f2ea3dc3655dd794dd2c2cfa12dc9c84b08
Glen Harold <glensilas95@gmail.com>
2022-08-07 20:29:44 -0400

    Configure mail app to use html template

--- utils/mailApp.py
+++ utils/mailApp.py
@@ -8,6 +8,10 @@ from email.message import EmailMessage
 
 # EMAIL_ADDRESS = 'daily-article@readspanishdaily.com'
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
 def sendEmail(url, diff, topic, bcc):
 #grab envVars locally or in prod
     path = ''
@@ -16,8 +20,7 @@ def sendEmail(url, diff, topic, bcc):
     elif os.name == 'posix':
         path = '/home/gharold/utils/env_vars.py'
 
-    exec(open(path).read())    
-    
+    exec(open(path).read()) 
     EMAIL_ADDRESS = os.environ.get('GMAIL')
     PASSWORD = os.environ.get('GMAIL_KEY')
     print('url: ')
@@ -29,40 +32,55 @@ def sendEmail(url, diff, topic, bcc):
     if len(bcc) > 0:
         try:
             with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-            	smtp.ehlo()
-            	smtp.starttls()
-            	smtp.ehlo()
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.ehlo()
 
-            	smtp.login(EMAIL_ADDRESS, PASSWORD)
-            	msg = EmailMessage()
-            	#if url is a list, then [0] is the URL and [1] is the substituted rating
-            	if type(url) == list:
-            	    print('closest article to ' + diff + ' sent')
-            	    realDiff = url[1]
-            	    url = url[0]
-            	    body = 'No `' + diff + '` articles found for: ' + topic + '. The closest we could find was this `' + realDiff + '` article here: ' + url
-            	else:
-            	    body = 'Enjoy this `' + diff + '` ' + topic + ' article: ' + url
+                smtp.login(EMAIL_ADDRESS, PASSWORD)
+                msg = EmailMessage()
+                #if url is a list, then [0] is the URL and [1] is the substituted rating
+# =============================================================================
+#                 if type(url) == list:
+#                     print('closest article to ' + diff + ' sent')
+#                     realDiff = url[1]
+#                     url = url[0]
+#                     body = 'No `' + diff + '` articles found for: ' + topic + '. The closest we could find was this `' + realDiff + '` article here: ' + url
+#                 else:
+#                     body = 'Enjoy this `' + diff + '` ' + topic + ' article: ' + url
+# =============================================================================
 
-                #set body
-            	msg.set_content(body)
-            	msg['Subject'] = 'Your Daily ' + topic + ' Article!'
-            	msg['From'] = EMAIL_ADDRESS
-            	msg['BCC'] = bcc
+                # Create the body of the message (a plain-text and an HTML version).
+                msg = MIMEMultipart('alternative')
+                msg['Subject'] = 'Your Daily ' + topic + ' Article!'
+                msg['From'] = EMAIL_ADDRESS
+                msg['BCC'] = "glensilas95@gmail.com"
+                
+                text = "this is the standard text string"
+                with open('utils/email_template.html') as f:
+                    html = f.read()
+                    html = html.replace("{{url}}", url)
 
+                # Record the MIME types of both parts - text/plain and text/html.
+                part1 = MIMEText(text, 'plain')
+                part2 = MIMEText(html, 'html')
+                
+                # Attach parts into message container.
+                # According to RFC 2046, the last part of a multipart message, in this case
+                # the HTML message, is best and preferred.
+                msg.attach(part1)
+                msg.attach(part2)
+                # Send the message via local SMTP server.
+                smtp.sendmail(EMAIL_ADDRESS, "glensilas95@gmail.com", msg.as_string())
 
-            	#subject = 'Test subject'
-            	#body = 'Test body'
+                print('Topic: ' + topic + ', diff: ' + diff + ' - sent to ' + str(len(bcc)) + ' users')
+                return 'Success'
 
-            	#msg = f'Subject: {subject}\n\n{body}'
-
-            	#smtp.sendmail(SENDER, RECEIVER, msg)
-            	smtp.send_message(msg)
-            	print('Topic: ' + topic + ', diff: ' + diff + ' - sent to ' + str(len(bcc)) + ' users')
-            	return 'Success'
         except:
-            print('There was an error, email not sent to: ' + ", ".join(bcc))
+           print('There was an error, email not sent to: ' + ", ".join(bcc))
             return 'error'
+
     else:
         print('No receipients for this topic/diff')
         return 'No recepient settings match'
+    
+    
